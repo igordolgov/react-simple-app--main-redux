3abf1cdb56fab760a460dad98c5e3c78f27749f7
Igor Dolgov <igordolgov1972@gmail.com>
2021-07-11 14:06:41 +0300

    added reducers

--- src/index.js
+++ src/index.js
@@ -1,93 +1,24 @@
 import React from 'react' // импортируем модуль React
 import ReactDOM from 'react-dom' // импортируем другой модуль React
-import TodoItem from './todo-item' // импортируем TodoItem (из файла todo-item.js)
+import TodoApp from './containers/app' // импортируем модуль app.js c именем TodoApp
+import { createStore } from 'redux'
+import todos from './reducers'
 
-// React-component основанный на классах (class-based)
-class TodoApp extends React.Component { // создаём компонент, который будет наследовать от React.Component (базовый класс для всех Реакт-компонентов)
-  constructor () { // объявляем у него конструктор (метод, который выполняется для создания экземпляра данного класса)
-    super() // выполняем родительский код конструктора из Реакт-компонента
-    // определим исходное состояние нашего приложения (оно должно храниться в специальном свойстве state,
-    this.state = { // как правило это объект)
-      todos: [ // пусть это будет массив, в котором будут три задачи (три объекта)
-        { name: 'Настроить webpack', checked: true },
-        { name: 'Запустить webpack-dev-server', checked: true },
-        { name: 'Написать TodoApp', checked: false }
-      ],
-      newTodoText: '' // и текст для новой задачи
-    }
-  }
+const initialState = [ // Зададим начальное состояние приложения
+  { id: 1, name: 'Настроить webpack', checked: true },
+  { id: 2, name: 'Запустить webpack-dev-server', checked: true },
+  { id: 3, name: 'Написать TodoApp', checked: false }
+]
 
-  toggleTodo (key) { // Создаём метод, который будет переключать классы у элементов списка
-    const todos = this.state.todos.map((todo, i) => { // Создаём локальную переменную todos и с помощью метода map() проверяем каждый элемент массива todos:
-      if (key === i) { // если это та задача, на которую мы кликнули (то есть, если ключ совпадает с её номером в массиве),
-        return { // то мы вернём такой объект -
-          name: todo.name, // name останется без изменений,
-          checked: !todo.checked // а checked поменяется на обратный
-        }
-      } else { // иначе вернём просто todo
-        return todo
-      }
-    })
-
-    // Обновляем состояние приложения:
-    this.setState({ todos }) // метод setState вызывает обновление состояния приложения и приложение рендерится с новыми данными
-  }
-
-  addTodo () { // создаём метод для добавления новых задач в список
-    const todos = this.state.todos
-    todos.push({ // добавляем новую задачу в конец списка
-      name: this.state.newTodoText,
-      checked: false
-    })
-
-    // Обновляем состояние приложения:
-    this.setState({
-      todos,
-      newTodoText: ''
-    })
-  }
-
-  render () { // для компонентов, основанных на классах, определяем метод render (в формате JSX),
-    return ( // который будет возвращать то, что этот компонент рендерит (список из трёх пунктов)
-      <div>
-        <h2>Список задач</h2>
-        <ol>
-          {
-            this.state.todos.map((todo, i) => {
-            // Создаём переменную className и с помощью метода map() проверяем каждый элемент массива todos. Если в элементе есть свойство "checked", то присваиваем переменной className значение "checked", иначе присваиваем значение "" (ничего не присваиваем)
-              return (
-                <TodoItem // из файла todo-item.js
-                  key={i}
-                  name={todo.name}
-                  checked={todo.checked}
-                  toggleTodo={this.toggleTodo.bind(this, i)} // привяжем toggleTodo к текущему контексту
-                />
-              )
-            })
-          }
-        </ol>
-
-        <input
-          type='text'
-          placeholder='Новая задача'
-          value={this.state.newTodoText}
-          onChange={ev => { // при любых изменениях (при вводе текста в input)
-            this.setState({ newTodoText: ev.target.value }) // отображать этот текст в поле input
-          }}
-          onKeyUp={ev => {
-            if (ev.key === 'Enter') { // если нажата клавиша Enter
-              this.addTodo() // выполняем функцию addTodo
-            }
-          }}
-        />
-      </div>
-    )
-  }
-}
+// Создаём хранилище (store) и передаём ему функцию-редьюсер (первый аргумент: название
+// редьюсера, а второй: начальное состояние нашего приложения
+const store = createStore(todos, initialState) // функция будет вызываться каждый раз,
+// когда в приложении возникает событие (action)
 
 // Указываем куда на странице мы хотим зарендерить наш компонент:
 ReactDOM.render( // метод получает на вход ДВА параметра:
-// <HelloWorld />, // то, что нужно зарендерить,
-  <TodoApp />, // то, что нужно зарендерить,
-  document.querySelector('#app') // и куда на странице это нужно поместить (в тег с id "app")
+// <HelloWorld />, // то, что нужно зарендерить...
+  <TodoApp store={store} />, // Так как приложение не имеет своего состояния,
+  // в качестве пропса передаём ему store
+  document.querySelector('#app') // ...и куда на странице это поместить (в тег с id "app")
 )
